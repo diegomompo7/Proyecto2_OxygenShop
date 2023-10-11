4a8fea14355970fa2fb7514f4566d809383910fe
diegomompo7 <diego.mompo.redoli@gmail.com>
2023-10-11 17:13:39 +0200

    Button Return to top

--- js/main.js
+++ js/main.js
@@ -1,11 +1,41 @@
-const scroll = document.createElement("hr")
-let width = 0%
 
+// SCROLLBAR
+
+
+const scroll = document.createElement("hr")
 scroll.setAttribute('style', 'border: 3px solid red; position:fixed')
+
+
+document.onscroll = () => {
+    height = window.scrollY / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+    scroll.width = `${height*100}%`
+}
 
 document.body.insertBefore(scroll, document.querySelector(".header"))
 
-document.onscroll = () => {
-    const height = window.scrollY / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-    scroll.width = `${height*100}%`
+//-------------------------------------------------------------------------------------------------
+
+// BUTTON RETURN TO THE TOP
+
+const scrollContainer = document.createElement("div")
+const scrollUp = document.createElement("button")
+scrollContainer.classList.add("btn")
+scrollUp.classList.add("btn__return")
+scrollUp.textContent = "Return to the top"
+
+document.body.insertBefore(scrollContainer, document.querySelector(".footer"))
+scrollContainer.append(scrollUp)
+
+scrollUp.onclick = () => {
+    let toTop = window.setInterval(() => {
+        let pos = window.scrollY;
+        if(pos > 0){
+            scrollTo(0, pos-100)
+        } else {
+            window.clearInterval(toTop);
+        }
+    },200)
+
 }
+
+//-----------------------------------------------------------------------------------------------------
